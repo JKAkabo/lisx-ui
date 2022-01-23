f4b2f6b4a911d71803bc3aa909a4b30385a94b49
John Kennedy Akabo <akabojohnkennedy@gmail.com>
2022-01-23 22:00:08 +0000

    feat: add navigation drawer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app>
-        <v-app-bar app color="white">
-            <v-app-bar-nav-icon/>
+        <v-app-bar color="white" app>
+            <v-app-bar-nav-icon @click="nav = true"/>
             <v-app-bar-title>lisx</v-app-bar-title>
             <v-spacer/>
             <h3>{{$store.state.title}}</h3>
@@ -10,6 +10,34 @@
                 <span class="font-weight-bold">JK</span>
             </v-avatar>
         </v-app-bar>
+        <v-navigation-drawer v-model="nav" app temporary>
+            <v-list nav>
+                <v-list-item :to="{name: 'Users'}" exact>
+                    <v-list-item-icon>
+                        <v-icon>mdi-account-group</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>Users</v-list-item-title>
+                </v-list-item>
+                <v-list-item :to="{name: 'APIKeys'}" exact>
+                    <v-list-item-icon>
+                        <v-icon>mdi-key-chain</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>API Keys</v-list-item-title>
+                </v-list-item>
+                <v-list-item :to="{name: 'Home'}" exact>
+                    <v-list-item-icon>
+                        <v-icon>mdi-slot-machine</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>Analyzers</v-list-item-title>
+                </v-list-item>
+                <v-list-item :to="{name: 'Home'}" exact>
+                    <v-list-item-icon>
+                        <v-icon>mdi-clipboard-text</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>Results</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
         <v-main>
             <router-view/>
         </v-main>
@@ -21,7 +49,10 @@
 
 <script>
 export default {
-    name: 'Home'
+    name: 'Home',
+    data: () => ({
+        nav: false
+    })
 };
 </script>
 
